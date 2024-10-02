92f31531ab86132932ff2b1d608f56a41c5d7302
axelode <endangtryna56@gmail.com>
2024-10-02 17:55:39 +0700

    initial

--- deepClone.js
+++ deepClone.js
@@ -7,19 +7,14 @@ const original = {
 };
 
 function deepClone(obj) {
-    // Jika nilai adalah null atau bukan objek, langsung kembalikan nilainya
     if (obj === null || typeof obj !== 'object') {
         return obj;
     }
 
-    // Buat array atau objek baru untuk menyimpan salinan
     const clone = Array.isArray(obj) ? [] : {};
 
-    // Salin properti satu per satu
     for (let key in obj) {
-        // Hanya salin properti yang benar-benar dimiliki objek
         if (obj.hasOwnProperty(key)) {
-            // Rekursi: jika properti adalah objek atau array, clone secara mendalam
             clone[key] = deepClone(obj[key]);
         }
     }
@@ -28,7 +23,7 @@ function deepClone(obj) {
 }
 
 const cloned = deepClone(original);
-cloned.b.d[2].e = 6; // Ubah salah satu nilai di salinan
+cloned.b.d[2].e = 6;
 
-console.log(original.b.d[2].e); // Output: 5 (tidak berubah karena deep clone)
-console.log(cloned.b.d[2].e);   // Output: 6 (salinan berubah)
+console.log(original.b.d[2].e);
+console.log(cloned.b.d[2].e);
